1cfcc117e253c517a157396ecb8621eab6f8f9c2
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-05-05 17:12:52 +0200

    Mise en place de la migration et de la conenxion vers la base avec toutes les tables identitys

--- KronoGeo_Api/Program.cs
+++ KronoGeo_Api/Program.cs
@@ -1,14 +1,16 @@
 using KronoGeo_Api.Applications.ExtendMethods;
+using KronoGeo_Api.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-// prochaine etape 
-// - installer Npgsql.EntityFrameworkCore.PostgreSQL et créer le dbcontext -- fait
-// - mettre en place l'authentification
-// - mettre en place les tests unitaires -- fait
-
+// - prochaine etape
+// - installer Npgsql.EntityFrameworkCore.PostgreSQL et créer le dbcontext -- fait table dans la base de données fait 
+// - il va falloir mettre en place un systeme pour prendre en compte le systeme d'enregistrement de la chaine de connexion dans les variable d'environnmement et le tester 
+// - mettre en place l'authentification - manque toute la partie JWT bearer et enregistrement des param aussi
+// - mettre en place les tests unitaires -- projet créé 
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -22,6 +24,21 @@ builder.Services.AddSwaggerGen();
 #region Mise en place de MediatR
 // - method d'extension
 builder.Services.AddMediaTRExtend();
+#endregion
+
+#region DbContext
+// - récupération de la ligne de connexion vers la base de données -- stocker dans les données secrètes
+string? stringConnection = builder.Configuration.GetConnectionString("DefaultConnection") ??
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found");
+
+// - appel du DbContext pour la connexion vers la base
+builder.Services.AddDbContext<KronoGeoDbContext>(options =>
+    options.UseNpgsql(stringConnection));
+#endregion
+
+#region Authentification / bearer Jwt
+// mise en place de Identity.Ui
+builder.Services.AddCustonIdentityUser();
 #endregion
 
 var app = builder.Build();
@@ -42,6 +59,7 @@ if (app.Environment.IsStaging() || app.Environment.IsDevelopment() || app.Enviro
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
