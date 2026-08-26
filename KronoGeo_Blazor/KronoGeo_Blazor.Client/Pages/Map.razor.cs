37b5e096923f2a718bc307beaf74d399f6decfb4
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-08-26 13:02:08 +0200

    Mise en de Mapsui attention avec le render mode en Assembly - et conpiler le code pour qu'il soit charger pour  le client

--- KronoGeo_Blazor/KronoGeo_Blazor.Client/Pages/Map.razor.cs
+++ KronoGeo_Blazor/KronoGeo_Blazor.Client/Pages/Map.razor.cs
@@ -1,8 +1,20 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Mapsui.UI.Blazor;
+using Microsoft.AspNetCore.Components;
 
 namespace KronoGeo_Blazor.Client.Pages
 {
     public class MapBase : ComponentBase
     {
+        protected MapControl? MapControl { get; set; }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            if (firstRender)
+            {
+                if ( MapControl != null )
+                    MapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+            }
+        }
     }
 }
