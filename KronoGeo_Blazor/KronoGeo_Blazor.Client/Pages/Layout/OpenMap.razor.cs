69aac2e280982b747d47ab6b78016958b4bca15a
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-08-27 10:39:26 +0200

    creation de la page OpenMap.razor qui va gérer l'affichage de la map avec les options

---
+++ KronoGeo_Blazor/KronoGeo_Blazor.Client/Pages/Layout/OpenMap.razor.cs
@@ -0,0 +1,23 @@
+﻿using Mapsui.UI.Blazor;
+using Microsoft.AspNetCore.Components;
+
+namespace KronoGeo_Blazor.Client.Pages.Layout
+{
+    public class OpenMapBase: ComponentBase
+    {
+        #region protected properties
+        protected MapControl? MapControl;
+        #endregion
+
+        #region protected override method
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+            if (firstRender)
+            {
+                MapControl?.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+            }
+        }
+        #endregion
+    }
+}
--- KronoGeo_Blazor/KronoGeo_Blazor.Client/Pages/Map.razor.cs
+++ KronoGeo_Blazor/KronoGeo_Blazor.Client/Pages/Map.razor.cs
@@ -5,16 +5,6 @@ namespace KronoGeo_Blazor.Client.Pages
 {
     public class MapBase : ComponentBase
     {
-        protected MapControl? MapControl { get; set; }
-
-        protected override void OnAfterRender(bool firstRender)
-        {
-            base.OnAfterRender(firstRender);
-            if (firstRender)
-            {
-                if ( MapControl != null )
-                    MapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
-            }
-        }
+        
     }
 }
