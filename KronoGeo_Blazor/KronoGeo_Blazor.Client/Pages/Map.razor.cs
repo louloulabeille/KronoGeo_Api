f0499c6b6d4bf1e63906fc9fa9d74f77eb766e56
Aurélien Loudiéres <louloulabeille@hotmail.com>
2026-09-23 12:55:43 +0200

    Mise en place d'un filtre de recherche dans sur les group de Localisation

--- KronoGeo_Blazor/KronoGeo_Blazor.Client/Pages/Map.razor.cs
+++ KronoGeo_Blazor/KronoGeo_Blazor.Client/Pages/Map.razor.cs
@@ -6,6 +6,7 @@ using Mapsui.UI.Blazor;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.EntityFrameworkCore.ValueGeneration;
 
 namespace KronoGeo_Blazor.Client.Pages
 {
@@ -23,23 +24,28 @@ namespace KronoGeo_Blazor.Client.Pages
 
         #region protected properties view
         protected bool IsLoading { get; set; } = false;
+        // -- recherche dans les groupes de localisations
+        protected string Search { get; set; } = string.Empty;
+        
+
+        /// <summary>
+        /// liste des groupes de localisation de l'utilisateur connecté
+        /// par page de 10 éléments par défaut
+        /// avec le filtre search ou non
+        /// </summary>
+        protected IEnumerable<LocalisationGroup>? DataOnView => _filterLocalisationGroup?
+            .OrderByDescending(lg => lg.Date).Skip((PageActuel - 1) * _pageSize).Take(_pageSize).ToList();
+
 
         // -- pagination calcul
         protected int PageActuel = 1;
 
         /// <summary>
-        /// liste des groupes de localisation de l'utilisateur connecté
-        /// par page de 10 éléments par défaut
-        /// </summary>
-        protected IEnumerable<LocalisationGroup>? DataOnView => _localisationGroup?
-            .OrderByDescending(lg => lg.Date).Skip((PageActuel - 1) * _pageSize).Take(_pageSize).ToList();
-        
-        /// <summary>
         /// nombre total de page pour la pagination
         /// nombre / pageSize + 1 si reste > 0 calcul avec le modulo
         /// </summary>
         protected int TotalPages =>
-            (_localisationGroup?.Count ?? 0) / _pageSize + ((_localisationGroup?.Count ?? 0) % _pageSize > 0 ? 1 : 0);
+            (_filterLocalisationGroup?.Count ?? 0) / _pageSize + ((_filterLocalisationGroup?.Count ?? 0) % _pageSize > 0 ? 1 : 0);
 
         /// <summary>
         /// indique si le bouton précédent est désactivé ou non
@@ -61,6 +67,8 @@ namespace KronoGeo_Blazor.Client.Pages
         /// </summary>
         private List<LocalisationGroup>? _localisationGroup { get; set; } = [];
 
+        private List<LocalisationGroup>? _filterLocalisationGroup { get; set; } = [];
+
         // -- nombre d'éléments par page
         private readonly int _pageSize = 10;
         #endregion
@@ -70,7 +78,10 @@ namespace KronoGeo_Blazor.Client.Pages
         {
             IsLoading = true; // -- mise en place du loader trop rapide le chargement
             await GetLoadGroupLocationAsync();
-            
+
+            // -- initialisation du filter List avec tous les enregistrements
+            _filterLocalisationGroup = _localisationGroup;
+
             await base.OnInitializedAsync();
         }
 
@@ -83,10 +94,18 @@ namespace KronoGeo_Blazor.Client.Pages
             }
             base.OnAfterRender(firstRender);
         }
+        #endregion
 
+        #region public method event
+        protected async Task FilterSearch()
+        {
+            PageActuel = 1;
+            _filterLocalisationGroup = _localisationGroup?.Where(w => w.Name.ToLower().Contains(Search.ToLower())).ToList();
+        }
         #endregion
 
         #region protected method
+
         protected void PreviousPage()
         {
             if (PageActuel > 1)
@@ -120,6 +139,8 @@ namespace KronoGeo_Blazor.Client.Pages
             _mapState?.OpenMapwithLocalisations(group.Localisations);
 
         }
+
+
         #endregion
 
         #region private method 
